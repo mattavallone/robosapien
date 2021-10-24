7657c6de149cd25490f264458d93718bb9673f84
Matthew Avallone <mva271@nyu.edu>
2021-10-24 19:25:18 -0400

    refactored IR codes into separate file

---
+++ ir_codes.py
@@ -0,0 +1,52 @@
+CODE_RSTurnRight       = 0x80
+CODE_RSRightArmUp      = 0x81
+CODE_RSRightArmOut     = 0x82
+CODE_RSTiltBodyRight   = 0x83
+CODE_RSRightArmDown    = 0x84
+CODE_RSRightArmIn      = 0x85
+CODE_RSWalkForward     = 0x86
+CODE_RSWalkBackward    = 0x87
+CODE_RSTurnLeft        = 0x88
+CODE_RSLeftArmUp       = 0x89
+CODE_RSLeftArmOut      = 0x8A
+CODE_RSTiltBodyLeft    = 0x8B
+CODE_RSLeftArmDown     = 0x8C
+CODE_RSLeftArmIn       = 0x8D
+CODE_RSStop            = 0x8E
+
+CODE_RSRightTurnStop   = 0xA0
+CODE_RSRightHandThump  = 0xA1
+CODE_RSRightHandThrow  = 0xA2
+CODE_RSSleep           = 0xA3
+CODE_RSRightHandPickup = 0xA4
+CODE_RSLeanBackward    = 0xA5
+CODE_RSForwardStep     = 0xA6
+CODE_RSBackwardStep    = 0xA7
+CODE_RSLeftTurnStep    = 0xA8
+CODE_RSLeftHandThump   = 0xA9
+CODE_RSLeftHandThrow   = 0xAA
+CODE_RSListen          = 0xAB
+CODE_RSLeftHandPickup  = 0xAC
+CODE_RSLeanForward     = 0xAD
+CODE_RSReset           = 0xAE
+
+CODE_RSExecute         = 0xB0
+CODE_RSWakeUp          = 0xB1
+
+CODE_RSRightHandStrike3 = 0xC0
+CODE_RSRightHandSweep   = 0xC1
+CODE_RSBurp             = 0xC2
+CODE_RSRightHandStrike2 = 0xC3
+CODE_RSHigh5            = 0xC4
+CODE_RSRightHandStrike1 = 0xC5
+CODE_RSBulldozer        = 0xC6
+CODE_RSOops             = 0xC7
+CODE_RSLeftHandStrike3  = 0xC8
+CODE_RSRightHandSweep   = 0xC9
+CODE_RSWhistle          = 0xCA
+CODE_RSLeftHandStrike2  = 0xCB
+CODE_RSTalkback         = 0xCC
+CODE_RSLeftHandStrike1  = 0xCD
+CODE_RSRoar             = 0xCE
+
+CODE_RSNoOp            = 0xEF
